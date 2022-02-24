11e4f9c8026492a7dcb5f6c564f1a2576e7330ae
Victor Giraldello <victor.giraldello@gmail.com>
2022-02-23 21:00:16 -0300

    Ribbon load balancing

---
+++ hr-payroll/src/main/java/com/vsoft/hrpayroll/config/AppConfig.java
@@ -0,0 +1,10 @@
+package com.vsoft.hrpayroll.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+    
+}
