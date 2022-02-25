84df5fe45319f2c997e44f0a8a3b67184e1b53b1
Victor Giraldello <victor.giraldello@gmail.com>
2022-02-24 21:12:34 -0300

    Random port to hr-payroll

--- hr-payroll/src/main/java/com/vsoft/hrpayroll/resouces/PaymentResource.java
+++ hr-payroll/src/main/java/com/vsoft/hrpayroll/resouces/PaymentResource.java
@@ -1,5 +1,6 @@
 package com.vsoft.hrpayroll.resouces;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.vsoft.hrpayroll.entities.Payment;
 import com.vsoft.hrpayroll.service.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,9 +17,15 @@ public class PaymentResource {
     @Autowired
     private PaymentService service;
 
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
     @GetMapping(value = "/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
         Payment payment = service.getPayment(workerId, days);
+        return ResponseEntity.ok(payment);
+    }
+
+    public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days) {
+        Payment payment = new Payment("Brann", 400.0, days);
         return ResponseEntity.ok(payment);
     }
 }
