2c348db3bd283c32942cc64384215481ac8df47e
Victor Giraldello <victor.giraldello@gmail.com>
2022-02-25 20:22:20 -0300

    hr-worker as a config server client

--- hr-worker/src/main/java/com/vsoft/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/vsoft/hrworker/resources/WorkerResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 
 @RestController
 @RequestMapping(value = "/workers")
@@ -20,11 +21,20 @@ public class WorkerResource {
 
     private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 
+    @Value("${test.config}")
+    private String testConfig;
+
     @Autowired
     private Environment env;
 
     @Autowired
     private WorkerRepository repository;
+
+    @GetMapping(value = "/configs")
+    public ResponseEntity<Void> getConfigs() {
+        logger.info("CONFIG = " + testConfig);
+        return ResponseEntity.noContent().build();
+    }
 
     @GetMapping
     public ResponseEntity<List<Worker>> findAll() {
@@ -35,13 +45,13 @@ public class WorkerResource {
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id) {
 
-        /*
+
 		try {
 			Thread.sleep(3000L);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		*/
+
 
         logger.info("PORT = " + env.getProperty("local.server.port"));
         Worker obj = repository.findById(id).get();
